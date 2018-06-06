a8a3564a76c1f443cec2eade46cba33cc3683beb
Jeffrey Ricker <jeffrey.ricker@gmail.com>
2018-06-05 19:28:26 -0500

    Added DeleteCheesesViewModel, refactored; still working on AddEdit view model and refactoring/debugging

---
+++ CheeseMVC/ViewModels/AddEditCheeseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CheeseMVC.ViewModels
+{
+    public class AddEditCheeseViewModel
+    {
+        public AddEditCheeseViewModel()
+        {
+        }
+    }
+}
---
+++ CheeseMVC/ViewModels/DeleteCheesesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheeseMVC.ViewModels
+{
+    public class DeletCheeseViewModel
+    {
+        public List<Cheese> Cheeses { get; set; }
+
+        public List<int> SelectedCheeses { get; set;  }
+    }
+}
